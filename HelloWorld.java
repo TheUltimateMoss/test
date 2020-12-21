74a5664d463b494145701fac079ab4cc3e2b2272
TheUltimateMoss <69699186+TheUltimateMoss@users.noreply.github.com>
2020-12-21 17:32:31 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String args[]) {
+		int[] A= {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		for (int i=0; i < A.length; i++) {
+		    System.out.println("Egg mayonase concentration levels:  "+A[i]+"%");
+		}
+	}
+}
---
+++ Mayonnaise.java
@@ -0,0 +1,8 @@
+public class Mayonnaise{
+	public static void main(String args[]) {
+		int[] x= {10, 20, 30, 40, 50 , 60 , 70 , 80, 90, 100};
+		for(int y=0;y<x.length;y++) {
+			System.out.println("Egg Mayonnaise Concentration Level:  "+x[y]+"%");
+		}
+	}
+}
