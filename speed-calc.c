a085fda198eb17fc926a07007bdc6168a1d9faad
TheUltimateMoss <69699186+TheUltimateMoss@users.noreply.github.com>
2021-03-01 11:45:49 +0000

    Create speed-calc.c
    INCOMPLETE

---
+++ speed-calc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+{
+    int s[0];
+    int d[0];
+    int t[0];
+
+    printf( "enter time:  ");
+    gets( t );
+
+    printf("enter distance:  ");
+    gets( d );
+    
+    
+    //int a;
+    //int b;
+    int c;
+    
+    //a=d;
+    //b=t;
+    //c=a/b;
+    
+    printf("%d", c);
+}
