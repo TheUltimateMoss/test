b7af5ed3b984acaeff14dc1985f40fc8c289a258
TheUltimateMoss <69699186+TheUltimateMoss@users.noreply.github.com>
2021-03-01 09:33:10 +0000

    Update joke.c

--- joke.c
+++ joke.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+void main( ) {
+   int enter;
+   printf( "Why do programmers make bad drivers?");
+   enter = getchar( );
+   printf( "\nbecuause they ignore all the warnings");
+   putchar(enter);
+}
