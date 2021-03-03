04c07eec389aaaab8f8dd6437d063b2bea992cb6
TheUltimateMoss <69699186+TheUltimateMoss@users.noreply.github.com>
2021-03-03 14:28:30 +0000

    Create speed-calc2.c

---
+++ speed-calc2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int speed;
+    int time;
+    int distance;
+    
+    distance=6;
+    time=2;
+    
+    
+    speed=distance/time;
+    
+    printf("%d", speed);
+}
